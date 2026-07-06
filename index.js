969be0d1a0f2ac842064b72057cc8b4cd13cb9f1
wanjirumuthike18 <wanjirumuthike18@gmail.com>
2026-07-06 16:42:02 +0300

    Complete Flatbook DOM manipulation lab

--- index.js
+++ index.js
@@ -42,6 +42,24 @@ const bookStore = {
         }
     ]
 }
+const bookStoreTitle = document.getElementById('header');
+bookStoreTitle.textContent = bookStore.name;
 
-// Write your code here!
+const bookList = document.getElementById('book-list');
+bookList.innerHTML = ''; // remove the example li before appending real books
+
+for (const book of bookStore.books) {
+  const bookContainer = document.createElement('li');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('p');
+  const bookImage = document.createElement('img');
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookImage.src = book.imageUrl;
+
+  bookContainer.append(bookTitle, bookAuthor, bookImage);
+  bookList.append(bookContainer);
+}
+
 
